fe99aa4d96d4803d227497089508dad9c7a345a5
sidiqbimo <sani21bimo@gmail.com>
2024-05-07 19:56:12 +0700

    able to run but cannot add note yet

--- app/src/main/java/com/bimobelajar/mynoterev/MainActivity.kt
+++ app/src/main/java/com/bimobelajar/mynoterev/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.bimobelajar.mynoterev
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
